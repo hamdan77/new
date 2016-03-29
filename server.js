032e990f4d92c708b3853e6f0d603869411e84b2
hamdan77 <hamdanwajahat@hotmail.com>
2016-03-29 23:56:35 +0500

    Added files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+                    res.writeHead(200, {'Content-Type' : 'text/html'}) ;          res.end('
+<html lang="en">+
+
+
+    
+<p style="color:lightgreen"> Heroku is a cloud Platform-as-a-Service (PaaS)  Heroku was acquired by Salesforce.com in 2010. It supports programming languages like<b> Java, Node.js, Scala, Clojure, Python,  PHP</b>. </p>
+');
+                               
+                               });
+
+
+
+var port = Number(process.env.PORT || 3000);
+server.listen(port);
