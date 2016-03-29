af8df74f69dde7fe95c4b3aab80e191a1a57d7b8
hamdan77 <hamdanwajahat@hotmail.com>
2016-03-30 00:01:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var http = require('http');
 
 var server = http.createServer(function(req,res){
                     res.writeHead(200, {'Content-Type' : 'text/html'}) ;          res.end('
-<html lang="en">+
+
 
 
     
-<p > Heroku is a cloud Platform-as-a-Service (PaaS)  Heroku was acquired by Salesforce.com in 2010. It supports programming languages like Java, Node.js, Scala, Clojure, Python,  PHP. </p>
+<p style="color:lightgreen" > Heroku is a cloud Platform-as-a-Service (PaaS)  Heroku was acquired by Salesforce.com in 2010. It supports programming languages like Java, Node.js, Scala, Clojure, Python,  PHP. </p>
 ');
                                
                                });
